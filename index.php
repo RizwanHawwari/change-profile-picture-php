17fa802d147078a7a4b924374271b10a4f415934
Rizwan Hawwari <rzwnhawari@gmail.com>
2024-09-06 17:16:54 +0700

    styling index page

--- index.php
+++ index.php
@@ -4,13 +4,22 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/styles.css">
   <title>Change Profile</title>
 </head>
 
 <body>
   <div class="container">
-
+    <div class="header">
+      <h1>Change Profile</h1>
+      <hr>
+    </div>
+    <div class="profile-picture">
+      <ul>
+        <li><img src="img/"></li>
+        <li>Username</li>
+      </ul>
+    </div>
   </div>
 </body>
 
